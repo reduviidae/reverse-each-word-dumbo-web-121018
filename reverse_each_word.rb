a673d50d44a4eb00aec3c1fbd4f5aa7c862ee59c
Wil Cook <wil.cook.is@gmail.com>
2018-11-22 20:47:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,5 +4,11 @@ def reverse_each_word(string)
   new_array.each do |word|
     working_array << word.reverse 
   end
-  return working_array.join(" ")
+  return working_array.join()
 end
+
+def collect_method(string)
+  new_array = strig.split
+  new_array.collect do |word|
+    word.reverse
+end  
